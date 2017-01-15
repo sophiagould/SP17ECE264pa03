1b5a0e79b8fb5922a275ef994530c5ab7defe5ec
Milind Kulkarni <milind@purdue.edu>
2017-01-14 21:38:42 -0500

    Initial files for PA03

---
+++ answer03.c
@@ -0,0 +1,20 @@
+#include "pa03.h"
+double integrate1(Range rng)
+{
+	//Perform numerical integration of the function func(x)
+	//INPUT: Range rng -- the lower and upper limits of the integration
+	//                    and the number of intervals to integrate over
+	//OUTPUT: Return the result of the integration
+
+}
+
+void integrate2(RangeAnswer * rngans)
+{
+	//Perform numerical integration of the function func(x)
+	//INPUT: RangeAnswer * rngans 
+	//              -- the lower and upper limits of the integration
+	//                 and the number of intervals to integrate over
+	//OUTPUT: The result of the integration should be placed in rngans->answer
+
+	//You should try to call integrate1 when implementing integrate2
+}
