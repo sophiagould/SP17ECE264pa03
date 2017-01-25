17465b9523178de87385e0413cea593457a56aee
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-01-24 20:05:54 -0500

    first

--- answer03.c
+++ answer03.c
@@ -1,6 +1,14 @@
 #include "pa03.h"
 double integrate1(Range rng)
 {
+	double result = 0;
+	double reg = 0;
+	reg = (rng.upperlimit - rng.lowerlimit) / (rng.intervals);
+	for (int i = 0; i < rng.intervals; i++){
+		result += reg * func(rng.lowerlimit + i * reg);
+	}
+	return result;
+
 	//Perform numerical integration of the function func(x)
 	//INPUT: Range rng -- the lower and upper limits of the integration
 	//                    and the number of intervals to integrate over
@@ -10,6 +18,7 @@ double integrate1(Range rng)
 
 void integrate2(RangeAnswer * rngans)
 {
+	rngans->answer = integrate1(rngans->rng);
 	//Perform numerical integration of the function func(x)
 	//INPUT: RangeAnswer * rngans 
 	//              -- the lower and upper limits of the integration
